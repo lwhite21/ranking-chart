8895d6042536d46e7ace14279bbe9077ff89e27d
Logan White <logan.white225@gmail.com>
2023-03-02 22:43:24 -0600

    initial commit

---
+++ src/Chart/Chart Title/ChartTitle.tsx
@@ -0,0 +1,7 @@
+import './ChartTitle.css'
+
+export function ChartTitle() {
+    return(
+        <div className='test'>hi</div>
+    );
+}
---
+++ src/Chart/Chart.tsx
@@ -0,0 +1,10 @@
+import { ChartTitle } from "./Chart Title/ChartTitle";
+import './Chart.css';
+
+export function Chart() {
+    return(
+        <div className="chart-overall-container">
+            <ChartTitle/>
+        </div>
+    );
+}
